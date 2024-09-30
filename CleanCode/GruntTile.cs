ea15d2309e4185eaca4039c59da083d7d832d50b
Looweez <louiselee2005@gmail.com>
2024-09-30 13:25:22 +0200

    enemy and some grunt

---
+++ CleanCode/GruntTile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode
+{
+    internal class GruntTile : EnemyTile
+    {
+        public GruntTile(Position position) : base(position, 10, 1)
+        {
+
+        }
+
+        public override char Display
+        {
+            get
+            {
+                if (IsDead == false)
+                {
+                    return 'Ϫ';
+                }
+                else
+                {
+                    return 'X';
+                }
+            }
+        }
+
+
+
+    }
+}
